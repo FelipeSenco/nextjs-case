43e63f0a21b97c0304d6cd55ae6745fbff019d41
Felipe de Senco <felipedesenco@gmail.com>
2023-11-07 16:10:58 -0500

    Styles for home page -add a bit of styles to the home page -added .env.local file to store secrets and more

--- src/app/Components/WeatherDisplay.tsx
+++ src/app/Components/WeatherDisplay.tsx
@@ -6,9 +6,11 @@ type WeatherDisplayProps = {
 
 const WeatherDisplay: FC<WeatherDisplayProps> = ({ data }) => {
   return (
-    <div>
-      Weather in Brasília is: {data.temp_c} degree celsius,{" "}
-      {data.condition.text}
+    <div className="text-center p-4 rounded bg-blue-50 border border-blue-200">
+      <p className="text-lg text-gray-700">Weather in Brasília:</p>
+      <p className="text-xl font-semibold text-gray-800">
+        {data.temp_c}° Celsius, {data.condition.text}
+      </p>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,14 @@ import { getWeatherData } from "@/utils/getWeather";
 export default async function Home() {
   const data = await getWeatherData();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Welcome to my NextJs case. Thank you for this opportunity!</p>
-      <WeatherDisplay data={data} />
+    <main className="flex min-h-screen p-24 items-center justify-center bg-gray-100">
+      <div className="flex flex-col bg-white rounded-lg shadow-lg p-10">
+        <h1 className="text-3xl font-semibold text-gray-800 mb-2">Welcome!</h1>
+        <p className="text-lg text-gray-600 mb-8">
+          Thank you for this opportunity to present my Next.js case.
+        </p>
+        <WeatherDisplay data={data} />
+      </div>
     </main>
   );
 }
