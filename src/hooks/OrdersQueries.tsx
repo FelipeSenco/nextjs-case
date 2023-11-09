47763baf8a0254b8fe953359b70d608aab11916e
Felipe de Senco <felipedesenco@gmail.com>
2023-11-09 04:49:23 -0500

    Optmistic updates -introduced optmistic updates on orders updates and deletes

--- src/hooks/OrdersQueries.tsx
+++ src/hooks/OrdersQueries.tsx
@@ -7,7 +7,8 @@ import {
   editOrder,
   getOrders,
 } from "@/utils/ordersRequests";
-import { useMutation, useQuery } from "react-query";
+import { Customer } from "@prisma/client";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 export const useOrdersQuery = (enabled = false) => {
   const { data, isError, isLoading, refetch } = useQuery(
@@ -36,27 +37,52 @@ export const useCreateOrderMutation = () => {
 };
 
 export const useEditOrderMutation = () => {
-  const { refetch } = useOrdersQuery();
+  const queryClient = useQueryClient();
+  const originalState = queryClient.getQueryData(["orders"]) as ExtendedOrder[];
+  const customers = queryClient.getQueryData(["customers"]) as Customer[];
 
   return useMutation(editOrder, {
+    onMutate: editedOrder => {
+      const optimisticState = originalState.map(order => {
+        //if the id match, update the fields for the order
+        if (order.id === editedOrder.id) {
+          const customer = customers.find(c => c.id === editedOrder.customerId);
+          order = {
+            ...order,
+            Customer: customer as Customer,
+            customerId: editedOrder.id,
+            quantity: editedOrder.quantity,
+            product: editedOrder.product,
+            dateCreated: new Date(),
+          };
+        }
+        return order;
+      });
+      //set the query data to the optimistic state
+      queryClient.setQueryData(["orders"], optimisticState);
+    },
     onError: (error: Error) => {
       console.log(error);
-    },
-    onSuccess: () => {
-      refetch();
+      //revert the query data to original, if the api fails
+      queryClient.setQueryData(["orders"], originalState);
     },
   });
 };
 
 export const useDeleteOrderMutation = () => {
-  const { refetch } = useOrdersQuery();
+  const queryClient = useQueryClient();
+  const originalState = queryClient.getQueryData(["orders"]) as ExtendedOrder[];
 
   return useMutation(deleteOrder, {
+    onMutate: id => {
+      //set the query data to the optimistic state
+      const optimisticState = originalState.filter(o => o.id !== id);
+      queryClient.setQueryData(["orders"], optimisticState);
+    },
     onError: (error: Error) => {
       console.log(error);
-    },
-    onSuccess: () => {
-      refetch();
+      //revert the query data to original, if the api fails
+      queryClient.setQueryData(["orders"], originalState);
     },
   });
 };
